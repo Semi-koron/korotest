6573a7e52bdd73d331d7a07089b2a33a36c52de9
Semi-koron <6jimoto6@gmail.com>
2024-05-14 20:09:46 +0900

    practice

--- src/app/ryoma/page.tsx
+++ src/app/ryoma/page.tsx
@@ -1,5 +1,24 @@
 import Link from "next/link";
 
+type UserData = {
+  UserId: string;
+  UserIcon: string;
+  DisplayName: string;
+  Follower: number;
+  Following: number;
+  Introduce: string;
+};
+
+const SemikoronData: UserData = {
+  UserId: "semikoron",
+  UserIcon:
+    "https://pbs.twimg.com/profile_images/1652730366892085248/djNLZzPt_400x400.jpg",
+  DisplayName: "Semikoron",
+  Follower: 100,
+  Following: 100,
+  Introduce: "I'm Semikoron",
+};
+
 export default function Home() {
   return (
     <>
